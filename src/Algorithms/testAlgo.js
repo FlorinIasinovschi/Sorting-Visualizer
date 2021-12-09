4efb8d8635e53ef6d0ec0f056e204831a500467c
Florin Iasinovschi <mineloom@gmail.com>
2021-12-09 23:56:36 +0200

    Added first algorithm, need to make it wait for the loop to finish

---
+++ src/Algorithms/testAlgo.js
@@ -0,0 +1,23 @@
+export function sortAlgo(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(i);
+    // if (arr[i] > arr[i + 1]) {
+    //   let temp = arr[i + 1];
+    //   arr[i + 1] = arr[i];
+    //   arr[i] = temp;
+
+    //   while (arr[i] < arr[i - 1]) {
+    //     let temp2 = arr[i - 1];
+    //     arr[i - 1] = arr[i];
+    //     arr[i] = temp2;
+    //   }
+    // }
+
+
+  }
+
+
+  return newArr;
+}
+
--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './SortingVisualizer.css';
+import { sortAlgo } from "../Algorithms/testAlgo";
 
 export class SortingVisualizer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      arr: []
+      arr: [],
+      debugState: "Nothing"
     }
   }
 
@@ -20,14 +22,45 @@ export class SortingVisualizer extends React.Component {
     }
     this.setState({ arr: newArr })
   }
+
+  sortingAlgo() {
+    const jsAlgo = this.state.arr.slice().sort((a, b) => a - b);
+    const testAlgo = sortAlgo(this.state.arr.slice());
+    console.log(testAlgo);
+    console.log(correctSort(jsAlgo, testAlgo));
+    // this.setState({
+    //   arr: sortAlgo(this.state.arr)
+    //debugState: sortAlgo(this.state.arr)
+    //})
+  }
+
   render() {
     return (
-      <div id="bars-container">
-        {this.state.arr.map((val, indx) => <div key={indx} className="bars" style={{ height: `${val}px` }}></div>)
-        }
+      <div className="main-container">
+        <div id="bars-container">
+          {this.state.arr.map((val, indx) => <div key={indx} className="bars" style={{ height: `${val}px` }}></div>)
+          }
+        </div>
+        <div>{this.state.debugState}</div>
+        <button onClick={() => this.createArray()}>Generate New Array</button>
+
+        <button onClick={() => this.sortingAlgo()}>Sorting Algorithm</button>
       </div>
+
     )
   }
+}
+
+function correctSort(js, test) {
+
+  for (let i = 0; i < 150; i++) {
+    if (js[i] != test[i]) {
+      console.log(js[i]);
+      console.log(test[i]);
+      return false;
+    }
+  }
+  return true;
 }
 
 function rndN(min, max) {
